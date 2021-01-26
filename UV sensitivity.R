d71f1d9797679d818e91708973b9c458746db3a5
MBender1992 <mbender1992@web.de>
2021-01-26 17:56:51 +0100

    Update

--- UV sensitivity.R
+++ UV sensitivity.R
@@ -9,6 +9,7 @@ library(devtools)
 # source R functions
 source_url("https://raw.githubusercontent.com/MBender1992/base_scripts/Marc/R_functions.R")  
 
+    
 
 #############################
 #                           #
@@ -21,20 +22,10 @@ url_file <- "https://raw.githubusercontent.com/MBender1992/PhD/Marc/Data/200619_
 dat <-  load_Fireplex_data_PhD(filename = url(url_file), threshold = 2.5)
 
 # summarize data
-dat_summarized <- dat %>% 
-  group_by(cell_line, Irradiation, miRNA) %>% 
-  summarize(mean = mean(expression)) %>%
-  ungroup() %>% 
-  group_by(cell_line, miRNA) %>%
-  spread(Irradiation, mean) %>%
-  mutate(FC = ifelse(is.infinite(KAUVIR/control), NA, KAUVIR/control)) %>%
-  mutate(logFC = ifelse(is.infinite(log2(FC)), NA, log2(FC))) %>%
-  mutate(abslogFC = abs(logFC)) %>% 
-  ungroup() %>% 
-  mutate(miRNA = str_replace_all(.$miRNA, "hsa_","")) %>%
-  mutate(miRNA = str_replace_all(.$miRNA, "_","-")) 
+dat_UV_sens <- folds %>% 
+  ungroup()%>% 
+  mutate(abslog2FC= abs(log2FC))
   
-
 
 #################################
 #                               #
@@ -47,42 +38,52 @@ dat_summarized <- dat %>%
 # Checking assumptions #
 ########################
 
+# check data
+dat_UV_sens %>% 
+  ggplot(aes(cell_line, abslog2FC)) +
+  geom_boxplot()
+
 # check for outliers 
-dat_summarized %>% 
+dat_UV_sens %>% 
   group_by(cell_line) %>%
-  identify_outliers(abslogFC)
+  identify_outliers(abslog2FC)
 
 # normality assumption
-model <- lm(abslogFC~cell_line, data = dat_summarized)
+model <- lm(abslog2FC~cell_line, data = dat_UV_sens)
 ggqqplot(residuals(model))
 
 # Compute Shapiro-Wilk test of normality
 shapiro_test(residuals(model))
 
 # shapiro test on each group
-dat_summarized %>%
+dat_UV_sens %>%
   group_by(cell_line) %>%
-  shapiro_test(abslogFC)
+  shapiro_test(abslog2FC)
 
 # qqplot for each group
-ggqqplot(dat_summarized, "abslogFC", ggtheme = theme_bw()) +
+ggqqplot(dat_UV_sens, "abslog2FC", ggtheme = theme_bw()) +
   facet_wrap(~ cell_line)
 
 # homogeneity of variance assumption
-dat_summarized %>% 
-  levene_test(abslogFC~cell_line)
+dat_UV_sens %>% 
+  levene_test(abslog2FC~cell_line)
 
 
 
 #### assumption of normality and homogeneity of variance violated
 
 # ..................................................................................................................................
-# dat_summarized %>% welch_anova_test(abslogFC~cell_line)
-# pvals <- dat_summarized %>% 
-#   games_howell_test(abslogFC~cell_line)
+# dat_UV_sens %>% welch_anova_test(abslog2FC~cell_line)
+# pvals <- dat_UV_sens %>% 
+#   games_howell_test(abslog2FC~cell_line)
 
-dat_summarized %>% 
-  wilcox_test(abslogFC~cell_line, p.adjust.method = "bonferroni")
+# Kruskal Wallis test to test if there are any differences beteen cell lines
+dat_UV_sens %>% 
+  kruskal_test(abslog2FC~cell_line)
+
+# Wilcoxon-Mann-Whitney U test to compare groups pairwise
+dat_UV_sens %>% 
+  wilcox_test(abslog2FC~cell_line, p.adjust.method = "fdr")
 
 
 ################
@@ -97,13 +98,13 @@ dat_summarized %>%
 pos <- position_jitter(width = 0.05, seed = 2)
 
 # define labels
-labels <- dat_summarized %>% 
+labels <- dat_UV_sens %>% 
   group_by(cell_line) %>%
-  top_n(2, abslogFC) %>% 
+  top_n(3, abslog2FC) %>% 
   mutate(label = miRNA)
 
 # merge data with labels
-dat_labelled <- left_join(dat_summarized,labels) %>%
+dat_labelled <- left_join(dat_UV_sens,labels) %>%
   mutate(label = ifelse(is.na(label), "", label))
 
 # open png device
@@ -111,7 +112,7 @@ png("test.png", units="in", width=5.5, height=6, res=600)
 
 # plot
 dat_labelled %>% 
-  ggplot(aes(cell_line,abslogFC)) +
+  ggplot(aes(cell_line,abslog2FC)) +
   stat_boxplot(
     geom ='errorbar',
     size=0.5,
@@ -119,26 +120,26 @@ dat_labelled %>%
   ) +
   geom_boxplot(
     color = "black",
-    mapping = aes(x = cell_line,y = abslogFC),
+    mapping = aes(x = cell_line,y = abslog2FC),
     outlier.shape =NA,
     width=0.4
   ) +
   geom_jitter(
     data =  filter(dat_labelled, label == ""),
-    mapping = aes(x = cell_line,y = abslogFC),
+    mapping = aes(x = cell_line,y = abslog2FC),
     width=0.1,
     alpha=0.3
   ) +
   geom_jitter(
     data =  filter(dat_labelled, label != ""),
-    mapping = aes(x = cell_line,y = abslogFC, fill = "red"),
+    mapping = aes(x = cell_line,y = abslog2FC, fill = "red"),
     shape = 21, 
     colour = "black",
     position = pos
   ) + 
   geom_text_repel(
     data =  filter(dat_labelled, label != ""),
-    mapping = aes(x = cell_line, y = abslogFC, label = label),
+    mapping = aes(x = cell_line, y = abslog2FC, label = label),
     size = 3,
     position = pos
   ) + 
